4f36a5fb7f7dbe4282f7d7a224fe3c8c02c302fa
Thang Nguyen <thangqnguyen@gmail.co>
2024-06-25 22:28:44 -0700

    Implemented sub-folders scripts

--- slideshow.py
+++ slideshow.py
@@ -4,7 +4,7 @@ import os
 
 print("Slideshow is running soon...")
 time.sleep(10)
-os.system("find E:/media \( -path E:/media/Europe -o -path E:/media/Collections \) -prune -o -type f -name '*.jpg' -print / feh -Z --auto-rotate -F -B black -z -r -x -Y -q -D 7")
+os.system("feh -Z --auto-rotate -F -B black -z -r -x -Y -q -D 7 ")
 
 while 1:
   time.sleep(1)
